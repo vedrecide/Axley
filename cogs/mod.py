224705cf304fafb78d959e72d52886b551443388
Lollipop <mohammedrayyan112233@gmail.com>
2021-06-01 15:28:36 +0530

    Added Purge command without content Lol

--- cogs/mod.py
+++ cogs/mod.py
@@ -11,11 +11,26 @@ class Moderation(commands.Cog):
     @commands.guild_only()
     @commands.bot_has_guild_permissions(manage_messages=True)
     @commands.has_permissions(manage_messages=True)
-    async def purge(self, amount: int, member: discord.Member = None):
+    async def purge(self, ctx, amount: int, member: discord.Member = None):
         if member == None:
-            pass # will work on this
+            try:
+                if amount <= -1:
+                    await ctx.message.reply('The amount must not be negative..', mention_author=False)
+                else:
+                    await ctx.channel.purge(limit=amount+1)
+            except ValueError:
+                await ctx.message.reply('The amount must be an natural number', mention_author=False)
         else:
-            pass
+            try:
+                if amount <= -1:
+                    await ctx.message.reply('The amount must not be negative..', mention_author=False)
+                else:
+                    def check(m):
+                        return m.author == member
+
+                    await ctx.channel.purge(limit=amount+1, check=check)
+            except ValueError:
+                await ctx.message.reply('The amount must be an natural number', mention_author=False)
 
 def setup(bot):
     bot.add_cog(Moderation(bot))

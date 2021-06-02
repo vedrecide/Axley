3e1bb85baa0c31f7dde82f98c55cff1f7318a188
Lollipop <mohammedrayyan112233@gmail.com>
2021-06-02 20:23:06 +0530

    Eh

--- cogs/moderation.py
+++ cogs/moderation.py
@@ -6,7 +6,6 @@ class Moderation(commands.Cog):
 
     def __init__(self, bot):
         self.bot = bot
-        self.emojis = self.bot.emojis()
 
     @commands.command(name='Purge', description='Purges a given amount of messages mentioned, also works with mentioning members at the end')
     @commands.guild_only()
@@ -21,7 +20,7 @@ class Moderation(commands.Cog):
                     await ctx.channel.purge(limit=amount+1)
                     embed = discord.Embed(
                         color=discord.Color.dark_red(),
-                        description='{} Purged `{}` messages'.format(self.emojis['tick'], amount)
+                        description='{} Purged `{}` messages'.format(self.bot.emojis['tick'], amount)
                     )
                     await ctx.send(embed=embed)
             except ValueError:
@@ -35,7 +34,11 @@ class Moderation(commands.Cog):
                         return m.author == member
 
                     await ctx.channel.purge(limit=amount, check=check)
-                    await ctx.send('{} Purged `{}` from **{}**'.format(self.emojis['tick'], amount, member))
+                    embed = discord.Embed(
+                        color=discord.Color.dark_red(),
+                        description='{} Purged `{}` messages from **{}**'.format(self.bot.emojis['tick'], amount, member)
+                    )
+                    await ctx.send(embed=embed)
             except ValueError:
                 await ctx.message.reply('The amount must be an natural number', mention_author=False)
 
